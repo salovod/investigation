6ae3ac436d2f3862268dcba46edb86f6600591da
ruslan.bilyi <ruslan.bilyi@lobyco.com>
2025-02-01 22:33:31 +0200

    fixing pipeline

--- SeleniumGoogleTest/com/configuration/browser/BrowserConfig.cs
+++ SeleniumGoogleTest/com/configuration/browser/BrowserConfig.cs
@@ -3,16 +3,19 @@ using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace SeleniumGoogleTest.com.configuration.browser;
-
-public class BrowserConfig
+namespace SeleniumGoogleTest.com.configuration.browser
 {
-    public IWebDriver CreateDriver()
+    public class BrowserConfig
     {
-        var options = new ChromeOptions();
-        options.AddArgument("--disable-blink-features=AutomationControlled");
-        var userDataDir = Path.Combine(Path.GetTempPath(), "ChromeUserData", Guid.NewGuid().ToString());
-        options.AddArguments($"--user-data-dir={userDataDir}");
-        return new ChromeDriver(options);
+        public IWebDriver CreateDriver()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+
+            var userDataDir = Path.Combine(Path.GetTempPath(), "ChromeUserData", $"{Guid.NewGuid()}_{DateTime.Now.Ticks}");
+            options.AddArguments($"--user-data-dir={userDataDir}");
+
+            return new ChromeDriver(options);
+        }
     }
 }
